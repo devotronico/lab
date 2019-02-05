7812500db0566298ad29a922117cc033b9541f06
Daniele Manzi <dmanzi83@gmail.com>
2019-02-05 21:06:56 +0100

    commit #4

--- index.php
+++ index.php
@@ -15,31 +15,80 @@ $root = "/workspace/LAB-02/";
     <title>LAB</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row py-5 text-center display-4">
+
+<div class="container">
+  <div class="d-flex align-items-center my-3 p-3 text-white-50 bg-primary rounded shadow-sm">
+    <img class="mr-3" src="image/flask-solid.svg" alt="lab" width="48" height="48">
+    <div class="lh-100">
+      <h6 class="mb-0 text-white lh-100">Laboratory</h6>
+      <small>Daniele Manzi</small>
+    </div>
+  </div>
+  </div>
+
+    <div class="container h4">
+        <div class="row p-5 text-center">
             <div class="col-3"><a href="test-01/index.php" title="Tooltip on top"><i class="fas fa-flask">01</i></a>&nbsp</div>
             <div class="col-3"><a href="test-02/index.php" title="Tooltip"><i class="fas fa-flask">02</i></a>&nbsp</div>
             <div class="col-3"><a href="test-03/index.php" title="LAB LIBERO"><i class="fas fa-flask">03</i></a>&nbsp</div>
             <div class="col-3"><a href="test-04/index.php" title="Tooltip"><i class="fas fa-flask">04</i></a>&nbsp</div>
         </div>
-        <div class="row py-5 text-center display-4">
+        <div class="row p-5 text-center">
             <div class="col-3"><a href="test-05/index.php" title="Closure"><i class="fas fa-flask">05</i></a>&nbsp</div>
             <div class="col-3"><a href="test-06/index.php" title="Lambda"><i class="fas fa-flask">06</i></a>&nbsp</div>
             <div class="col-3"><a href="test-07/index.php" title="SETUP VSC"><i class="fas fa-flask">07</i></a>&nbsp</div>
             <div class="col-3"><a href="test-08/index.php" title="STATIC VARIABLE"><i class="fas fa-flask">08</i></a>&nbsp</div>
         </div>
-        <div class="row py-5 text-center display-4">
+        <div class="row p-5 text-center">
             <div class="col-3"><a href="test-09/index.php" title="array_map"><i class="fas fa-flask">09</i></a>&nbsp</div>
             <div class="col-3"><a href="test-10/index.php" title="array_reduce"><i class="fas fa-flask">10</i></a>&nbsp</div>
             <div class="col-3"><a href="test-11/index.php" title="array_filter"><i class="fas fa-flask">11</i></a>&nbsp</div>
             <div class="col-3"><a href="test-12/index.php" title="array_reduce"><i class="fas fa-flask">12</i></a>&nbsp</div>
         </div>
-        <div class="row py-5 text-center display-4">
+        <div class="row p-5 text-center">
             <div class="col-3"><a href="test-13/index.php" title="array_walk"><i class="fas fa-flask">13</i></a>&nbsp</div>
             <div class="col-3"><a href="test-14/index.php" title="Tooltip"><i class="fas fa-flask">14</i></a>&nbsp</div>
             <div class="col-3"><a href="test-15/index.php" title="Tooltip"><i class="fas fa-flask">15</i></a>&nbsp</div>
             <div class="col-3"><a href="test-16/index.php" title="Tooltip"><i class="fas fa-flask">16</i></a>&nbsp</div>
         </div>
+        <div class="row p-5 text-center">
+            <div class="col-3"><a href="test-17/index.php" title="array_walk"><i class="fas fa-flask">17</i></a>&nbsp</div>
+            <div class="col-3"><a href="test-18/index.php" title="Tooltip"><i class="fas fa-flask">18</i></a>&nbsp</div>
+            <div class="col-3"><a href="test-19/index.php" title="Tooltip"><i class="fas fa-flask">19</i></a>&nbsp</div>
+            <div class="col-3"><a href="test-20/index.php" title="Tooltip"><i class="fas fa-flask">20</i></a>&nbsp</div>
+        </div>
     </div>
+
+
+
+  <div class="container">
+  <div class="my-3 p-3 bg-white rounded shadow-sm">
+    <h6 class="border-bottom border-gray pb-2 mb-0">Recent updates</h6>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#007bff" width="100%" height="100%"/><text fill="#007bff" dy=".3em" x="50%" y="50%">32x32</text></svg>
+      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <strong class="d-block text-gray-dark">@username</strong>
+        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+      </p>
+    </div>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#e83e8c" width="100%" height="100%"/><text fill="#e83e8c" dy=".3em" x="50%" y="50%">32x32</text></svg>
+      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <strong class="d-block text-gray-dark">@username</strong>
+        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+      </p>
+    </div>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#6f42c1" width="100%" height="100%"/><text fill="#6f42c1" dy=".3em" x="50%" y="50%">32x32</text></svg>
+      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <strong class="d-block text-gray-dark">@username</strong>
+        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+      </p>
+    </div>
+    <small class="d-block text-right mt-3">
+      <a href="#">All updates</a>
+    </small>
+  </div>
+  </div>
 </body>
 </html>
--- test-18/index.php
+++ test-18/index.php
@@ -2,10 +2,26 @@
 echo "<h1><a href='/LAB/index.php'><</a>&nbspTEST-13</h1>";
 
 
-echo "EMPTY";
+echo "ist-files-in-directory";
 echo "<br><br>";
 
 
+/**
+ * Legge tutte le cartelle e i file contenute nel progetto
+ */
+$rootDir = __DIR__; // __DIR__ = C:\xampp\htdocs\CodeWall
+
+$currentDirectoryItems = array_diff(scandir($rootDir . "/", 1), [".", ".."]); // Use array_diff to remove both period values eg: ("." , "..")
+$allFiles = [];
+foreach ($currentDirectoryItems as $value) {
+    $allFiles[] = $value;
+
+    if (is_dir($rootDir . "/" . $value)) { // Check if specified path is a directory
+        $allFiles[] = array_diff(scandir($rootDir . "/" . $value), [".", ".."]); // Use array_diff to remove both period values eg: ("." , "..");
+    }
+}
+// print_r($allFiles);
 
 
+echo "<pre>";print_r($allFiles);echo "</pre>"; // printa tutte le cartelle e i file
 
