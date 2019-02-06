80ddbd9c54ba9cbc0513759f9452129ca56c97cf
ElmetelDev <supporto3@elmetel.it>
2019-02-06 17:36:18 +0100

    commit #5

--- test-19/index.php
+++ test-19/index.php
@@ -6,5 +6,48 @@ echo "<br><br>";
 
 
 
+/*
+http://sdw.ecb.europa.eu/curConverter.do?
+sourceAmount=1.0&
+sourceCurrency=USD&
+targetCurrency=EUR&
+inputDate=06-02-2019&
+submitConvert.x=14&
+submitConvert.y=4
+
+*/
+
+
+$quantità = 1.0;
+$codice = "USD";
+$data = date("d-m-Y");
+
+$url = "http://sdw.ecb.europa.eu/curConverter.do?sourceAmount=$quantità&sourceCurrency=$codice&targetCurrency=EUR&inputDate=$data&submitConvert.x=44&submitConvert.y=8";
+// $url = "http://sdw.ecb.europa.eu/curConverter.do?sourceAmount=1.0&sourceCurrency=$codice&targetCurrency=EUR&inputDate=$data&submitConvert.x=44&submitConvert.y=8";
+// $url = "http://sdw.ecb.europa.eu/curConverter.do?sourceAmount=1.0&sourceCurrency=$codice&targetCurrency=EUR&inputDate=06-02-2019&submitConvert.x=44&submitConvert.y=8";
+// $url = "http://sdw.ecb.europa.eu/curConverter.do?sourceAmount=1.0&sourceCurrency=USD&targetCurrency=EUR&inputDate=06-02-2019&submitConvert.x=44&submitConvert.y=8";
+
+$content = file_get_contents($url);
+
+
+$x = "1&nbsp;EUR&nbsp;=&nbsp;";
+$y = "&nbsp;$codice";
+// $y = "&nbsp;$codice</a></td>";
+
+
+
+
+$token1 = explode($x, $content);
+
+$token2 = explode($y, $token1[1]);
+
+
+echo $token2[0];
+
+//echo $content;
+
+
+
+
 
 
